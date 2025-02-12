1bf0ec39ddd579050132dbcbe78ae0c8222fc331
alperzkn <20475676+Alperzkn@users.noreply.github.com>
2025-02-12 19:30:20 +0300

    minor bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1663,7 +1663,7 @@ function App() {
                               sx={{
                                 position: 'absolute',
                                 top: 0,
-                                left 0,
+                                left: 0,
                                 right: 0,
                                 bottom: 0,
                                 zIndex: 2,
